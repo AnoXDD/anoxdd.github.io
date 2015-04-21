16e5b3fb18c340703a2de0a6ab41103f42e2321f
AnoXDD <guanrunjie@gmail.com>
2015-04-21 00:47:04 -0700

    042115 Minor fix

--- Journoxic/js/edit.js
+++ Journoxic/js/edit.js
@@ -705,6 +705,62 @@ edit.location = function(index) {
 				var latitude = position.coords.latitude,
 					longitude = position.coords.longitude;
 				pos = new google.maps.LatLng(latitude, longitude);
+				var mapOptions = {
+					zoom: 15,
+					center: pos,
+				}, map = new google.maps.Map(document.getElementById("map-selector"), mapOptions),
+				searchBox = new google.maps.places.SearchBox((document.getElementById('place-search')));
+
+				// [START region_getplaces]
+				// Listen for the event fired when the user selects an item from the
+				// pick list. Retrieve the matching places for that item.
+				google.maps.event.addListener(searchBox, 'places_changed', function() {
+					var places = searchBox.getPlaces();
+
+					if (places.length == 0) {
+						return;
+					}
+					for (var i = 0, marker; marker = markers[i]; i++) {
+						marker.setMap(null);
+					}
+
+					// For each place, get the icon, place name, and location.
+					markers = [];
+					var bounds = new google.maps.LatLngBounds();
+					for (var i = 0, place; place = places[i]; i++) {
+						var image = {
+							url: place.icon,
+							size: new google.maps.Size(71, 71),
+							origin: new google.maps.Point(0, 0),
+							anchor: new google.maps.Point(17, 34),
+							scaledSize: new google.maps.Size(25, 25)
+						};
+
+						// Create a marker for each place.
+						var marker = new google.maps.Marker({
+							map: map,
+							icon: image,
+							title: place.name,
+							position: place.geometry.location
+						});
+
+						markers.push(marker);
+
+						bounds.extend(place.geometry.location);
+					}
+
+					map.fitBounds(bounds);
+				});
+				// [END region_getplaces]
+
+				// Bias the SearchBox results towards places that are within the bounds of the
+				// current map's viewport.
+				google.maps.event.addListener(map, 'bounds_changed', function() {
+					var bounds = map.getBounds();
+					searchBox.setBounds(bounds);
+				});
+
+
 			}, function() {
 				alert(errorMsg);
 			});
@@ -712,66 +768,6 @@ edit.location = function(index) {
 			// Browser doesn't support Geolocation
 			alert(errorMsg);
 		}
-
-		var mapOptions = {
-			zoom: 15,
-			center: pos,
-		}, map = new google.maps.Map(document.getElementById("map-selector"), mapOptions),
-				input = /** @type {HTMLInputElement} */(
-			  document.getElementById('place-search'));
-		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-		var searchBox = new google.maps.places.SearchBox(
-		  /** @type {HTMLInputElement} */(input));
-
-		// [START region_getplaces]
-		// Listen for the event fired when the user selects an item from the
-		// pick list. Retrieve the matching places for that item.
-		google.maps.event.addListener(searchBox, 'places_changed', function() {
-			var places = searchBox.getPlaces();
-
-			if (places.length == 0) {
-				return;
-			}
-			for (var i = 0, marker; marker = markers[i]; i++) {
-				marker.setMap(null);
-			}
-
-			// For each place, get the icon, place name, and location.
-			markers = [];
-			var bounds = new google.maps.LatLngBounds();
-			for (var i = 0, place; place = places[i]; i++) {
-				var image = {
-					url: place.icon,
-					size: new google.maps.Size(71, 71),
-					origin: new google.maps.Point(0, 0),
-					anchor: new google.maps.Point(17, 34),
-					scaledSize: new google.maps.Size(25, 25)
-				};
-
-				// Create a marker for each place.
-				var marker = new google.maps.Marker({
-					map: map,
-					icon: image,
-					title: place.name,
-					position: place.geometry.location
-				});
-
-				markers.push(marker);
-
-				bounds.extend(place.geometry.location);
-			}
-
-			map.fitBounds(bounds);
-		});
-		// [END region_getplaces]
-
-		// Bias the SearchBox results towards places that are within the bounds of the
-		// current map's viewport.
-		google.maps.event.addListener(map, 'bounds_changed', function() {
-			var bounds = map.getBounds();
-			searchBox.setBounds(bounds);
-		});
 
 
 	}

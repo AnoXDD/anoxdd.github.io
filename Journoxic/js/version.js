5224b668064d47d4201b2524326892b3bf659fcb
AnoXDD <guanrunjie@gmail.com>
2015-06-27 19:06:31 +0800

    3.22.2 Debug push

--- Journoxic/js/version.js
+++ Journoxic/js/version.js
@@ -1,4 +1,4 @@
-﻿app.version.main = "v3.22.1 Build 062715_1852 [list_update]";
+﻿app.version.main = "v3.22.2 Build 062715_1906 [list_update]";
 
 // Version number show up
 $("#version").html(app.version.main);

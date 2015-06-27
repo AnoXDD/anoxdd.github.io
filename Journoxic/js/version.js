9d4e04f74725ead94a7093f588a9256c45a51cb3
AnoXDD <guanrunjie@gmail.com>
2015-06-27 19:38:59 +0800

    3.22.4 CSS beautified

--- Journoxic/js/version.js
+++ Journoxic/js/version.js
@@ -1,4 +1,4 @@
-﻿app.version.main = "v3.22.3 Build 062715_1931 [list_update]";
+﻿app.version.main = "v3.22.4 Build 062715_1938 [list_update]";
 
 // Version number show up
 $("#version").html(app.version.main);

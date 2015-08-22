886f9bbd85dd710e80e4c2246f6a0e28485c5e3f
Anoxic <guanrunjie@gmail.com>
2015-08-22 15:55:59 +0800

    H1.0.2 Add more stuffs

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-/**
+﻿/**
  * Version 1.0
  */
+var oldIndex = -1,
+	newIndex = -1,
+	timeout = [];
 $(document).ready(function() {
+	/* Iterator */
+	var i;
 	$(".contact").hover(function() {
 		var m = "runjie";
 		m = "guan" + m;
@@ -19,44 +24,63 @@ $(document).ready(function() {
 			$h2.addClass("show");
 		}, 3000 * (index + 1));
 	});
+	// Add page indicator for each page
+	var length = $("#intro ul").length;
+	for (i = 0; i !== length; ++i) {
+		$("#page").append("<span></span>");
+	}
+	var pause = false;
+	$("#page span").hover(function() {
+		newIndex = $(this).index();
+		pause = true;
+	}, function() {
+		pause = false;
+	});
+	// Repeatly check the change of page
+	setInterval(function() {
+		if (oldIndex !== newIndex) {
+			// Test for validity
+			if (newIndex >= 0 && newIndex < length) {
+				// Clear previous timeout
+				for (i = 0; i !== timeout.length; ++i) {
+					clearTimeout(timeout[i]);
+				}
+				timeout = [];
+				// Show the appropriate indicator on page
+				$("#page span").removeClass("selected");
+				$("#page span:nth-child(" + (newIndex + 1) + ")").addClass("selected");
+				$("#intro ul:nth-child(" + (oldIndex + 1) + ")").addClass("hide");
+				var subLength;
+				$("#intro ul:nth-child(" + (newIndex + 1) + ")").removeClass("hide").addClass("show").children().each(function(subIndex) {
+					subLength = subIndex;
+					var $thisSub = $(this);
+					timeout.push(setTimeout(function() {
+						$thisSub.addClass("show");
+					}, subIndex * 3000));
+				});
+				oldIndex = newIndex;
+				if (newIndex !== length - 1) {
+					timeout.push(setTimeout(function() {
+						++newIndex;
+					}, (subLength + 2) * 3000));
+				}
+			} else {
+				// Go to replay
+				newIndex = length - 1;
+			}
+		}
+	}, 100);
 	setTimeout(replay, ($("h2").length + 1) * 3000);
 });
 
 function replay() {
-	$("ul.show, li.show").removeClass("show hide");
+	oldIndex = -1;
+	newIndex = -1;
+	$("#intro ul:last-child").addClass("hide");
+	$(".wrapper").addClass("hide");
+	$("#page").addClass("show");
 	setTimeout(function() {
-		$(".wrapper").addClass("hide");
-
-		// Calculate the delay time
-		var delays = [0];
-		$("#intro ul").each(function(index) {
-			delays.push(delays[index] + 3000 * ($(this).children().length + 1));
-		});
-		setTimeout(function() {
-			/* Change the value to adjust the time for changes, 3000*(elem+1) */
-			var clear = [0, 0, 0, 0],
-				currentDisplay = -1;
-			$("#intro ul").each(function(index) {
-				var $this = $(this);
-				setTimeout(function() {
-					currentDisplay = index;
-				}, delays[index]);
-				clear[index] = setInterval(function() {
-					if (currentDisplay === index) {
-						$("ul.show").addClass("hide");
-						// Time to display it
-						$this.addClass("show").children().each(function(subIndex) {
-							var $thisSub = $(this);
-							setTimeout(function() {
-								$thisSub.addClass("show");
-							}, subIndex * 3000);
-						});
-						clearInterval(clear[index]);
-					}
-				}, 100);
-			});
-		}, 3000);
-
-	}, 1000);
-
+		$("ul.show, li.show").removeClass("show hide");
+		newIndex = 0;
+	}, 3000);
 }

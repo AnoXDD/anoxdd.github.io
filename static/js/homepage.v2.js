fbe019746c83ea05203e8dcc812519ed83324f25
Anoxic <guanrunjie@gmail.com>
2015-10-17 10:05:37 -0700

    H1.1.2 Animation done

--- static/js/homepage.v2.js
+++ static/js/homepage.v2.js
@@ -3,7 +3,7 @@
  */
 function showContent() {
 	// Add something before
-	var header = "<p>(C) 2015 Runjie Guan. All rights reserved.\n</p>"
+	var header = "<p>(C) 2015 Runjie Guan. All rights reserved.<br/>This website only supports modern browser. </p>"
 	setTimeout(function() {
 		// Add to header
 		$(header).prependTo(".console");
@@ -16,11 +16,16 @@ function showContent() {
 						$("#console-content").html(console.substr(0, i));
 						func(++i);
 					}, 200);
-				} else if (i === console.length+ 1) {
+				} else if (i === console.length + 1) {
 					// Carriage return
 					$("<br/>").appendTo($("#console-content"));
 					setTimeout(function() {
 						$(".console").addClass("finished");
+
+						// Start the incoming animation
+						setTimeout(function() {
+							animateElems();
+						}, 500);
 					}, 2000);
 				}
 			};
@@ -28,6 +33,41 @@ function showContent() {
 		// Start showing the content of the homepage
 		func(0);
 	}, 500);
+}
+
+/**
+ * Animates the elements
+ */
+function animateElems() {
+	// The link on the header
+	$("header a").each(function(i) {
+		var $this = $(this);
+		setTimeout(function() {
+			$this.css({
+				animation: "fadein-top .4s ease",
+				opacity: 1
+			});
+		}, 100 * i);
+	});
+
+	// The basic description
+	$("#main-frame li").each(function(i) {
+		var $this = $(this);
+		setTimeout(function() {
+			$this.css({
+				animation: "fadein-left 1s ease",
+				opacity: 1
+			});
+		}, 100 * i);
+	});
+}
+
+/**
+ * Initializes the separator
+ * @returns {} 
+ */
+function initSeparator() {
+
 }
 
 // You can start with the documents ready

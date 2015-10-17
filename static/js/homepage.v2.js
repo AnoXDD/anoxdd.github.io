4cd215699377d199a6a2dd60f73eb1a9d8b58c97
Anoxic <guanrunjie@gmail.com>
2015-10-16 21:55:04 -0700

    H1.1.1 Add new homepage!

---
+++ static/js/homepage.v2.js
@@ -0,0 +1,34 @@
+﻿/**
+ * Shows the content, including emulating the UNIX console input
+ */
+function showContent() {
+	// Add something before
+	var header = "<p>(C) 2015 Runjie Guan. All rights reserved.\n</p>"
+	setTimeout(function() {
+		// Add to header
+		$(header).prependTo(".console");
+
+		// Show console input
+		var console = "ls -a",
+			func = function(i) {
+				if (i !== console.length + 1) {
+					setTimeout(function() {
+						$("#console-content").html(console.substr(0, i));
+						func(++i);
+					}, 200);
+				} else if (i === console.length+ 1) {
+					// Carriage return
+					$("<br/>").appendTo($("#console-content"));
+					setTimeout(function() {
+						$(".console").addClass("finished");
+					}, 2000);
+				}
+			};
+
+		// Start showing the content of the homepage
+		func(0);
+	}, 500);
+}
+
+// You can start with the documents ready
+showContent();

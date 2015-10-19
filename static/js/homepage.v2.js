926d254e3f4eafcf6b50ec4f25ef5c37d0b0f93b
Anoxic <guanrunjie@gmail.com>
2015-10-18 21:56:50 -0700

    H1.1.5 Start building mobile site

--- static/js/homepage.v2.js
+++ static/js/homepage.v2.js
@@ -98,6 +98,61 @@ function enableResponsiveHeader() {
 }
 
 /**
+ * Animates a DOM object to its auto height
+ * @param {object} element - The element to adjust the height
+ */
+function animateHeightToAuto(element) {
+	$(element).each(function() {
+		var curHeight = $(this).height(),
+			autoHeight = $(this).css("height", "auto").height();
+
+		$(this).height(curHeight).animate({ height: autoHeight }, 0);
+	});
+}
+
+/**
+ * Animates a DOM object to its original height
+ * @param {object} element - The element to adjust the height
+ */
+function animateHeightToOriginal(element) {
+	$(element).each(function() {
+		$(this).height("");
+	});
+
+}
+
+/**
+ * Enables the ripple effect
+ */
+function enableRippleEffect() {
+	$(document).delegate("a:not(.no-ripple), input[type=checkbox] + label, input[type=radio] + label", "click", function(e) {
+		var d;
+		// Find if any ripple effect is in it
+		if ($(this).find(".ink").length === 0) {
+			$(this).prepend("<span class='ink'></span>");
+		}
+
+		// Select the only one ripple effect
+		var $ink = $(this).find(".ink");
+		$ink.removeClass("ripple-animate");
+
+		if (!$ink.height() && !$ink.width()) {
+			d = Math.max($(this).outerWidth(), $(this).outerHeight());
+			$ink.css({
+				height: d,
+				width: d
+			});
+		}
+
+		// Animate the stuffs
+		var x = e.pageX - $(this).offset().left - $ink.width() / 2;
+		var y = e.pageY - $(this).offset().top - $ink.height() / 2;
+
+		$ink.css({ top: y + "px", left: x + "px" }).addClass("ripple-animate");
+	});
+}
+
+/**
  * Enables the smooth scroll
  */
 function enableSmoothScroll() {
@@ -119,6 +174,35 @@ function enableSmoothScroll() {
 }
 
 /**
+ * Enables the toggle of project detail
+ * @returns {} 
+ */
+function enableToggleProjectDetail() {
+	$(".project-wrapper").each(function() {
+		// Prepend a toggle to each one
+		var $this = $(this),
+			$toggle = $("<a class='toggle'></a>"),
+			isExpand = false;
+
+		$toggle.prependTo($(this)).click(function() {
+			// Toggle the class
+			$(this).toggleClass("expand");
+
+			if (isExpand) {
+				// Hide the selection
+				animateHeightToOriginal($this);
+			} else {
+				// Show the selection
+				animateHeightToAuto($this);
+			}
+
+			// Change its status
+			isExpand = !isExpand;
+		});
+	});
+}
+
+/**
  * Initializes the separator
  * @returns {} 
  */
@@ -132,4 +216,6 @@ enableSmoothScroll();
 
 $(document).ready(function() {
 	enableResponsiveHeader();
+	enableToggleProjectDetail();
+	enableRippleEffect();
 })
